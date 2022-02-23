53d12c296363e85e17de36e378ed5e4dc058bdce
Md Tanzil <mdtanzil.py@gmail.com>
2022-02-24 03:24:06 +0600

    Install sorl thumbnail for Imagefield

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from sorl.thumbnail import ImageField
 
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150, blank=False, null=False)
+    image = ImageField()
 
     def __str__(self):
         return self.title
